7d961fc7fff5efd3fe84200007292d2f66047a7d
Lucas Tavares <lucastavares@localhost.localdomain>
2022-05-11 22:48:38 -0300

    Deu completamente errado esse CLI porque o package.json igonora as minhas flas... aaaaaaaaaaaaaaafffffffffffiiiiiiiiiiiiiiiiii

---
+++ src/CLI.ts
@@ -0,0 +1,60 @@
+type argsOptions = {
+  [abrv: string]: {
+    type?: string | number;
+    choice?: string[] | number[];
+    alias?: string;
+    default?: string | number;
+  };
+};
+
+export default class CLI {
+  constructor(private options: argsOptions) {
+    this.args = process.argv.slice(2).toString().replace(",", " ");
+
+    console.log(process.argv);
+  }
+
+  args: string;
+
+  get argv() {
+    return Object.keys(this.options).reduce(
+      (
+        acc: { [key: string]: string | number | null },
+        abrv: keyof argsOptions
+      ) => {
+        const value = this.findValue({
+          abrv,
+          alias: this.options[abrv]?.alias,
+        });
+        const result = { [abrv]: value };
+        const { alias } = this.options[abrv];
+
+        if (alias !== undefined) {
+          result[alias] = value;
+        }
+
+        return { ...acc, ...result };
+      },
+      {}
+    );
+  }
+
+  private findValue(keys: {
+    abrv: keyof argsOptions;
+    alias?: string;
+  }): string | number | null {
+    const regex = new RegExp(
+      `/(-${keys.abrv}${keys.alias ? `|--${keys.alias}` : ""})=(.+)/`
+    );
+
+    const match = this.args.match(regex);
+
+    console.log(match);
+
+    try {
+      return match![2];
+    } catch (error) {
+      return null;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { options } from "yargs";
-
 import { server, client } from "./resolvers";
+import CLI from "./CLI";
 
 const resolvers = {
   client: client,
@@ -8,13 +7,14 @@ const resolvers = {
 };
 
 async function createOption() {
-  const argv = await options({
+  const { argv } = new CLI({
     t: { choice: ["client", "server"], alias: "type", default: "server" },
     p: { type: "number", alias: "port", default: 3000 },
     a: { type: "string", alias: "address", default: "127.0.0.1" },
-  }).argv;
+  });
 
   resolvers[argv.t as keyof typeof resolvers];
+  console.log(argv);
 }
 
 createOption();
